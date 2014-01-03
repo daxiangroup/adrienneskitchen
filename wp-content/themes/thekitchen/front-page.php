f9708d346650c1d659ba92b97115113bacaa067f
tylers <tylers@nationalfibre.net>
2014-01-03 16:41:48 -0500

    Updates to the layouts of front page and category listing

--- wp-content/themes/thekitchen/front-page.php
+++ wp-content/themes/thekitchen/front-page.php
@@ -55,28 +55,15 @@
 
 <div id="side-panels" class="widget-area">
     <div class="content">
-        <div class="panel-banner"><span>Recent</span></div>
+        <div class="panel-banner stopper"><span>Recent</span></div>
         <div class="sub-navigation site-panel">
-            <?php ak_recipe_sub_navigation(); ?>
+            <?php ak_recent_recipes(); ?>
         </div>
 
-        <div class="panel-banner"><span>Vitals</span></div>
-        <div class="vitals site-panel">
-
-            Cuisine: <?php echo recipress_recipe('cuisine'); ?> <br />
-            Course: <?php echo recipress_recipe('course'); ?> <br />
-            Skill: <?php echo recipress_recipe('skill_level'); ?> <br />
-            Prep Time: <?php echo recipress_recipe('prep_time', 'mins'); ?> <br />
-            Cook Time: <?php echo recipress_recipe('cook_time', 'mins'); ?> <br />
-            Ready Time: <?php echo recipress_recipe('ready_time', 'mins'); ?> <br />
-            Yield: <?php echo recipress_recipe('yield'); ?> <br />
+        <div class="panel-banner"><span>Favourites</span></div>
+        <div class="favourite-recipes site-panel">
+            <?php ak_favourite_recipes(); ?>
         </div>
-
-        <div class="panel-banner"><span>Related Recipes</span></div>
-        <div class="related-recipes site-panel">
-            <?php ak_related_recipes($post->ID); ?>
-        </div>
-
     </div>
 </div><!-- #primary .widget-area -->
 
